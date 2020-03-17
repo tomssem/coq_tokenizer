5b04c0f1abe30f2b9d55c81ed8a6c5e42e9e618f
Tom N <tfwnicholson+1@gmail.com>
2020-03-17 11:40:25 +0900

    Implement some functor and applicative stuff

--- CoqIOTest.v
+++ CoqIOTest.v
@@ -2,24 +2,57 @@ Require Import Coq.Lists.List.
 Require Import Io.All.
 Require Import Io.System.All.
 Require Import ListString.All.
+Require Import ListString.Comparison.
+From Coq Require Import Arith.EqNat.
+
 
 Import ListNotations.
 Import C.Notations.
+
+Definition RawString := C.t System.effect LString.t.
 
 (** The classic Hello World program. *)
 Definition hello_world (argv : list LString.t) : C.t System.effect unit :=
   System.log (LString.s "Hello world!").
 
-Definition cat (argv : list LString.t) : C.t System.effect unit :=
-  match argv with
-  | [_; file_name] =>
-    let! content := System.read_file file_name in
-    match content with
-    | None => System.log (LString.s "Cannot read the file.")
-    | Some content => System.log content
-    end
-  | _ => System.log (LString.s "Expected one parameter")
+Definition read_file (file_name : LString.t) : C.t System.effect LString.t :=
+  let! file := System.read_file file_name in
+  match file with
+  | None => ret []
+  | Some contents => ret contents
   end.
 
-Definition main := Extraction.launch cat.
+Definition fmap {E : Effect.t} {A B:Type}
+           (f:A->B)
+           (a:C.t E A) : (C.t E B) :=
+  let! _a := a in ret (f _a).
+
+Definition apply {E : Effect.t} {A B : Type}
+          (f: C.t E (A -> B)) (a : C.t E A) : C.t E B :=
+  let! _f := f in 
+    let! _a := a in
+      ret (_f _a).
+  
+(* check to see if b is a prefix of b *)
+Fixpoint match_head (a : LString.t) (b : LString.t) : bool:=
+    match a, b with
+    | _, [] => true
+    | [], _ => false
+    | h1::t1, h2::t2 => if (Char.eqb h1 h2) then (match_head t1 t2) else false
+    end.
+
+Definition match_headS (a : RawString) (b : RawString) : C.t System.effect bool :=
+  apply (fmap match_head a) b.
+
+(* convert bool to list string *)
+Definition of_bool (a:bool) :=
+  if a then (LString.s "true") else (LString.s "false").
+
+Definition _main (argv : list LString.t) : C.t System.effect unit :=
+  let! content := read_file (LString.s "test/HelloWorld.txt") in
+    let result := match_head content (LString.s "Hellp") in
+      System.log (of_bool result).
+           
+
+Definition main := Extraction.launch _main.
 Extraction "main" main.
